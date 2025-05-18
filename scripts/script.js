9fe9e75055b8222510534da62e865179e2269228
ismagilova-aa <sufiaismagilova5@gmail.com>
2025-05-18 23:47:28 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-'use strict'
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () => {
     // * 1. Начало.
     // * 2. Получаем все элементы изображений с описанием.
@@ -13,11 +14,68 @@ document.addEventListener("DOMContentLoaded", () => {
     // *         3.3.2. Нет: продолжаем.
     // * 4. Конец.
     
-    const aboutImg = document.querySelector(".about__image");
-    aboutImg.addEventListener('mouseenter', () => {
-        console.log('Мышка наведена на изображение, показываем текст');
+    const teachersImg = document.querySelector(".teachers__image");
+    
+    teachersImgImg.forEach((item, index) => {
+        const teachersText = document.querySelectorAll('.teachers__desc');
+
+        item.addEventListener('mouseenter', () => {
+            item.style.opacity = 0.5;
+            teachersText[index].removeAttribute('hidden');
+        });
+
+        item.addEventListener('mouseleave', () => {
+            item.style.opacity = 1;
+            teachersText[index].setAttribute('hidden', true);
+        });
     });
 
 });
+
+/* 1. Динамический вывод карточек тегов. Часть 1 (Используем с данными) */
+const teachersContainer = document.querySelector(".teachers__list");
+
+if (trenersContainer) {
+    const dataTitleTreners= [
+        "Ким Маргарита Сергеевна",
+        "Иванова Елена Андреевна",
+        "Сидорова Яна Павловна",
+        "Ммарков Марк Маркович",
+    ];
+
+    const titleTeachers =
+         teachersContainer.querySelectorAll(".teachers__name");
+
+         titleTeachers.forEach((item, index) => {
+            item.textContent = dataTitleTeachers[index];
+        });
+ }
+
+/* 4. Появление модального окна*/
+const aboutButtonModal = document.querySelector(".about__button");
+const modalApplication = document.querySelector(".applications");
+    
+if (aboutButtonModal && modalApplication) {
+    aboutButtonModal.addEventListener("click", () => {
+        modalApplication.removeAttribute("hidden");
+    });
+}
+// Закрытие модального окна при коике вне его области
+window.addEventListener("click", (event) => {
+    if (event.target === modalApplication) {
+        modalApplication.setAttribute("hidden", true);
+    }
+});
+
+// Закрытие модального окна при коике на кнопку закрытия
+const closeModalButton = document.querySelector(".application__close");
+
+closeModalButton.addEventListener("click", () => {
+    modalApplication.setAttribute("hidden", true);
+});
+
+
+
+
 
 

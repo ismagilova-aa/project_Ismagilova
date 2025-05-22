6021d4632eb311f823f8b2769bf92b1e62df26f7
ismagilova-aa <sufiaismagilova5@gmail.com>
2025-05-22 13:41:12 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -191,7 +191,22 @@ const cardsImages = document.querySelector(".images");
                         });
                     }                    
                 });
+}
+
+
+const preloader = document.querySelector(".preloader");
+    const content = document.querySelector(".content");
+    if (preloader && content) {
+        setTimeout(() => {
+            preloader.style.opacity = "0";
+            preloader.style.visibility = "hidden";
+
+            content.style.display = "block";
+
+            preloader.remove();
+        }, 3000); 
     }
+
 
 
 

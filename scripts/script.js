f2ae225158d9f9fd5be8633f154f090b1143d6c3
ismagilova-aa <sufiaismagilova5@gmail.com>
2025-05-25 00:20:45 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -239,12 +239,9 @@ if (preloader && content) {
 }
 
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
 
